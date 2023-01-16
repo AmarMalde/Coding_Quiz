f5f82b4b5cf6adb3086137d2a8f4998c96f5d957
[Amar Malde] <105677445+AmarMalde@users.noreply.github.com>
2023-01-16 00:00:29 +0000

    added code to change questions and answers when an answer is clicked

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,54 +2,87 @@ var startButton = document.getElementById("start");
 var startScreenClass = document.getElementById("start-screen");
 var questionsScreen = document.getElementById("questions");
 var questionOnScreen = document.getElementById("question-title");
+var timerValue = document.getElementById("time");
 
-var AnswerButton1 = document.getElementById("option1");
-var AnswerButton2 = document.getElementById("option2");
-var AnswerButton3 = document.getElementById("option3");
-var AnswerButton4 = document.getElementById("option4");
+var answerButtonsDiv = document.getElementById("choicesButtons");
+
+var answerButton1 = document.getElementById("option1");
+var answerButton2 = document.getElementById("option2");
+var answerButton3 = document.getElementById("option3");
+var answerButton4 = document.getElementById("option4");
 
 
-
+var startTime = 90
 
 var questionIndex = 0;
 
-startButton.addEventListener("click", function() {
+startButton.addEventListener("click", function () {
+
+    setInterval(function () {
+
+        if (startTime == 90) {
+            //hides the start screen by removing the class start and adding hide
+            startScreenClass.classList.remove("start");
+            startScreenClass.classList.add("hide");
+
+            //displays the question screen by removing the class hide and adding start
+            questionsScreen.classList.remove("hide");
+            questionsScreen.classList.add("start");
+
+            // update the screen with the current questions and answers
+            updateQuestionScreen();
+        }
+
+        timerValue.textContent = startTime;
+        startTime--;
+
+        answerButtonsDiv.addEventListener("click", function (event) {
+            
+            if (event.target.matches(".answerButton")) {
+
+                if (event.target.getAttribute('id') != questionsObject[questionIndex].correctAnswer) {
+                    startTime -= 10;
+                }
+                console.log("clicked")
+                questionIndex++;
+                updateQuestionScreen();
+
+            }
+        });
+
+        //add 1 to question index (if quesion index = questionsObject.length then show final screen)
+        //rerun updateQuestionScreen
+
+        
+
+    }, 1000);
 
 
-    //hides the start screen by removing the class start and adding hide
-    startScreenClass.classList.remove("start");
-    startScreenClass.classList.add("hide");
+});
 
-    //displays the question screen by removing the class hide and adding start
-    questionsScreen.classList.remove("hide");
-    questionsScreen.classList.add("start");
-    
-    // this will store the current question as a string
 
-    var question = returnQuestions();
+function updateQuestionScreen() {
 
     //displaying the question on the screen
 
-    questionOnScreen.textContent = question
+    questionOnScreen.textContent = returnQuestions();
 
     //this will store the answers as a string in an array
     var answers = returnAnswers();
 
     //displaying the answers
 
-    AnswerButton1.textContent = answers[0]
-    AnswerButton2.textContent = answers[1]
-    AnswerButton3.textContent = answers[2]
-    AnswerButton4.textContent = answers[3]
+    // AnswerButton1.textContent = answers[0]
+    answerButton1.textContent = answers[0];
+    answerButton2.textContent = answers[1];
+    answerButton3.textContent = answers[2];
+    answerButton4.textContent = answers[3];
 
-    //after answer is given
-    questionIndex ++;
-
-});
+}
 
 function returnQuestions() {
     //return question using question index
-    return(questionsObject[questionIndex].question);
+    return (questionsObject[questionIndex].question);
 }
 
 function returnAnswers() {
@@ -61,6 +94,6 @@ function returnAnswers() {
         questionsObject[questionIndex].option3,
         questionsObject[questionIndex].option4
     ]
-    return(answerArray)
+    return (answerArray)
 }
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,15 +5,39 @@ var questionsObject = [
         option2: "curly brackets",
         option3: "normal parenthesis",
         option4: "you don't use brackets when using arrays",
-        answerindex1: 0
+        correctAnswer: "option1"
     },
     {
-        question2: "What is an integer",
+        question: "What is an integer",
         option1: "a letter",
         option2: "a number",
         option3: "True or False",
         option4: "there's no such thing!",
-        answerIndex2: 1
+        correctAnswer: "option2"
+    },
+    {
+        question: "What is 1+2",
+        option1: "1",
+        option2: "2",
+        option3: "3",
+        option4: "4",
+        correctAnswer: "option3"
+    },
+    {
+        question: "What is 1+2",
+        option1: "1",
+        option2: "2",
+        option3: "3",
+        option4: "4",
+        correctAnswer: "option3"
+    },
+    {
+        question: "What is 1+2",
+        option1: "1",
+        option2: "2",
+        option3: "3",
+        option4: "4",
+        correctAnswer: "option3"
     }
 ]
 
